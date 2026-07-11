b89445117d4131ea859489da5d5ee767dfb29da3
Sutradhar2071 <sutradharringku@gmail.com>
2026-07-11 12:52:45 +0600

    server run

---
+++ app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send({
+    success: true,
+    message: "FixItNow API Running 🚀",
+  });
+});
+
+export default app;
